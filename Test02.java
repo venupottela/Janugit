34d30f91229965eded5ed6e18c05a1089d115f6a
venu <gvenu350@gmail.com>
2022-08-24 17:10:55 +0530

    test 02 adding line

--- Test02.java
+++ Test02.java
@@ -2,5 +2,7 @@ public class Test02 {
 
 	public static void main(String[] args) {	
                 System.out.println("hii");
+                System.out.println("hii");
+                System.out.println("hii");
 	}
 }
